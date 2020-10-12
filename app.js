7d83f27352ba6b135e6efe8e1739d22ed06fae36
Gabriel Neves <gabriel.bneves@gmail.com>
2020-10-12 14:49:56 +0100

    get & populate

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-// Sotrage controller (last thing to do)
+// ******************
+// Storage controller
+// ******************
 
+// ***************
 // Item controller
+// ***************
+
 const ItemCtrl = (function() {
   // Item Constructor
   const Item = function(id, name, calories) {
@@ -12,6 +17,7 @@ const ItemCtrl = (function() {
   //data structure / state
   const data = {
     items: [
+      // maybe experiment with fetch api here?
       {id: 0, name: 'Steak Dinner', calories: 1200},
       {id: 1, name: 'Cookie', calories: 400},
       {id: 2, name: 'Eggs', calories: 300},
@@ -21,27 +27,57 @@ const ItemCtrl = (function() {
   }
 
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
   }
 })();
 
+// *************
 // UI controller
+// *************
+
 const UICtrl = (function() {
+  const UISelectors = {
+    itemList: '#item-list' // in case our IDs change along the production
+  }
 
   return {
-
+    populateItemList: function(items) {
+      let html = '';
+      items.forEach((item) => {
+        html += `
+          <li class="collection-item" id="item-${this.id}">
+            <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+              <i class="edit-item fas fa-edit right"></i>
+            </a>
+          </li>
+        `;
+      });
+      //Insert List Items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
   }
   
 })();
 
+// **************
 // App Controller
+// **************
 const App = (function(ItemCtrl, UICtrl) {  
   
   return {
     init: function() {
       console.log('Initiating app . . .');
+      // fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // populate list with items
+      UICtrl.populateItemList(items);
     }
   }
 
